f1c679c57e2e7e91f23e1d76347bab4ab765334d
Kristie Rodriguez <kristierodriguez@Kristies-MacBook-Pro.local>
2017-06-23 17:37:29 -0500

    Adding updates to sample calculator exercise.

--- public/js/calculator_exercise.js
+++ public/js/calculator_exercise.js
@@ -1,116 +1,271 @@
-var leftInput = document.getElementById("leftInput")
-var operator = document.getElementById("operator")
-var rightInput = document.getElementById("rightInput")
+var leftInput = document.getElementById("leftInput");
+var operatorInput = document.getElementById("operatorInput");
+var rightInput = document.getElementById("rightInput");
+var sum = document.getElementById("sum");
+var clearAll = document.getElementById("operatorall_clear");
+var answer;
 
+//_____________Adding Numbers and Operators______________//
+    
 
+ var numberz = document.getElementsByClassName("number");
+ for (var i = 0; i < numberz.length; i++){
+ 	numberz[i].addEventListener("click", addNumberToDisplay);
+ }
+var operatorz = document.getElementsByClassName("operator");
+for (var i=0; i <operatorz.length; i++){
+	operatorz[i].addEventListener("click", addNumberToDisplay);
+}
 
-var one = document.getElementById("one")
-one.addEventListener("click", function(){
-	leftInput.value = 1
-	rightInput.value = 1
-})
+//___________________________________________________//
 
-var two = document.getElementById("two")
-two.addEventListener("click", function(){
-	leftInput.value = 2
-	rightInput.value = 2
-})
-
-var three = document.getElementById("three")
-three.addEventListener("click", function(){
-	leftInput.value = 3
-	rightInput.value = 3
-})
-
-var four = document.getElementById("four")
-four.addEventListener("click", function(){
-	leftInput.value = 4
-	rightInput.value = 4
-})
-
-var five = document.getElementById("five")
-five.addEventListener("click", function(){
-	leftInput.value = 5
-	rightInput.value = 5
-})
-
-var six = document.getElementById("six")
-six.addEventListener("click", function(){
-	leftInput.value = 6
-	rightInput.value = 6
-})
-
-var seven = document.getElementById("seven")
-seven.addEventListener("click", function(){
-	leftInput.value = 7
-	rightInput.value = 7
-
-})
-
-var eight = document.getElementById("eight")
-eight.addEventListener("click", function(){
-	leftInput.value = 8
-	rightInput.value = 8
-})
-
-var nine = document.getElementById("nine")
-nine.addEventListener("click", function(){
-	leftInput.value = 9
-	rightInput.value = 9
-})
-
-var zero = document.getElementById("zero")
-zero.addEventListener("click", function(){
-	leftInput.value = 0
-	rightInput.value = 0
-})
-
-var decimal = document.getElementById("decimal")
-decimal.addEventListener("click", function(){
-	leftInput.value = "."
-	rightInput.value = "."
-})
-
-var add = document.getElementById("add")
-add.addEventListener("click", function(){
-	operator.value = "+"
-})
-
-var subt = document.getElementById("subt")
-subt.addEventListener("click", function(){
-	operator.value = "-"
-})
-
-var mult = document.getElementById("mult")
-mult.addEventListener("click", function(){
-	operator.value = "*"
-})
-
-var sum = document.getElementById("sum")
-sum.addEventListener("click", function(){
-	operator.value = "="
-})
-
-
-var clear_all = document.getElementById("clear_all")
-clear_all
-
-
-
-var operator;
-	function setValues()
-	{
-		leftInput = Number(document.getElementById("leftInput").value);
-		operator = Number(document.getElementById("operator").value);
+function addNumberToDisplay(){
+	if (this.className.includes("operator")){
+	 	operatorInput.setAttribute("value", this.value);
+	}else if (operatorInput.value != "" && leftInput.value != "") {
+		var displayValue = rightInput.getAttribute("value");
+		displayValue += this.value;
+		rightInput.setAttribute("value", displayValue);
+	}else if (this.className.includes("number")){
+		var displayValue = leftInput.getAttribute("value");
+		displayValue += this.value;
+		leftInput.setAttribute("value", displayValue);
 	}
-
-	function sum() {
-		setValues();
-		results = a+b;
-	}
+}
 
 
+//___________________________________________________//
+clearAll.addEventListener("click", function(){
+	leftInput.setAttribute("value", "");
+	operatorInput.setAttribute("value", "");
+	rightInput.setAttribute("value", "");
+});
 
+function calculateResults(){
+	var leftMathInput = parseFloat(leftInput.value);
+	var rightMathInput = parseFloat(rightInput.value);
+	
+	if (operatorInput.value == "+") {
+	answer = leftMathInput + rightMathInput;
+	clearCalc();
+}else if (operatorInput.value == "-"){
+	answer = leftMathInput - rightMathInput;
+	clearCalc();
+}else if (operatorInput.value == "*"){
+	answer = leftMathInput * rightMathInput;
+	clearCalc();
+}else if (operatorInput.value == "/"){
+	answer = leftMathInput / rightMathInput;
+	clearCalc();
+}
+
+};
+
+sum.addEventListener("click", calculateResults);
+
+// function executeMathOperation(){
+// 	operator.value = this.value;
+// }
+
+function clearCalc (){
+	leftInput.value = answer;
+	operatorInput.value = "";
+	rightInput.value = "";
+}
+
+// function emptyTextBox(){
+// operator.value = "";
+// rightInput.value = "";
+
+// }
+
+// clearAll.addEventListener("click", function(){
+// 	operator.value = "";
+	
+// });
+
+
+
+
+// var one = document.getElementById("btnone")
+// one.addEventListener("click", function(){
+// 	leftInput.value += 1
+	
+// })
+
+// var two = document.getElementById("btntwo")
+// two.addEventListener("click", function(){
+// 	leftInput.value += 2
+	
+// })
+
+// var three = document.getElementById("btnthree")
+// three.addEventListener("click", function(){
+// 	leftInput.value += 3
+	
+// })
+
+// var four = document.getElementById("btnfour")
+// four.addEventListener("click", function(){
+// 	leftInput.value += 4
+	
+// })
+
+// var five = document.getElementById("btnfive")
+// five.addEventListener("click", function(){
+// 	leftInput.value += 5
+	
+// })
+
+// var six = document.getElementById("btnsix")
+// six.addEventListener("click", function(){
+// 	leftInput.value += 6
+	
+// })
+
+// var seven = document.getElementById("btnseven")
+// seven.addEventListener("click", function(){
+// 	leftInput.value += 7
+	
+
+// })
+
+// var eight = document.getElementById("btneight")
+// eight.addEventListener("click", function(){
+// 	leftInput.value += 8
+	
+// })
+
+// var nine = document.getElementById("btnnine")
+// nine.addEventListener("click", function(){
+// 	leftInput.value += 9
+	
+// })
+
+// var zero = document.getElementById("btnzero")
+// zero.addEventListener("click", function(){
+// 	leftInput.value += 0
+	
+// })
+
+// var decimal = document.getElementById("btndecimal")
+// decimal.addEventListener("click", function(){
+// 	leftInput.value = "."
+
+// })
+//____________________Adding Math Symbols to Operator_____________//
+
+
+// var add = document.getElementById("operatoradd")
+// add.addEventListener("click", function(){
+// 	operator.value = "+"
+// })
+
+// var subt = document.getElementById("operatorsubt")
+// subt.addEventListener("click", function(){
+// 	operator.value = "-"
+// })
+
+// var mult = document.getElementById("operatormult")
+// mult.addEventListener("click", function(){
+// 	operator.value = "*"
+// })
+
+// var sum = document.getElementById("operatorsum")
+// sum.addEventListener("click", function(){
+// 	operator.value = "="
+// })
+
+
+//_________________Adding Numbers to RightInput_____________________//
+
+// var one = document.getElementById("btnone")
+// one.addEventListener("click", function(){
+// 	rightInput.value += 1
+	
+// })
+
+// var two = document.getElementById("btntwo")
+// two.addEventListener("click", function(){
+// 	rightInput.value += 2
+	
+// })
+
+// var three = document.getElementById("btnthree")
+// three.addEventListener("click", function(){
+// 	rightInput.value += 3
+	
+// })
+
+// var four = document.getElementById("btnfour")
+// four.addEventListener("click", function(){
+// 	rightInput.value += 4
+	
+// })
+
+// var five = document.getElementById("btnfive")
+// five.addEventListener("click", function(){
+// 	rightInput.value += 5
+	
+// })
+
+// var six = document.getElementById("btnsix")
+// six.addEventListener("click", function(){
+// 	rightInput.value += 6
+	
+// })
+
+// var seven = document.getElementById("btnseven")
+// seven.addEventListener("click", function(){
+// 	rightInput.value += 7
+	
+
+// })
+
+// var eight = document.getElementById("btneight")
+// eight.addEventListener("click", function(){
+// 	rightInput.value += 8
+	
+// })
+
+// var nine = document.getElementById("btnnine")
+// nine.addEventListener("click", function(){
+// 	rightInput.value += 9
+	
+// })
+
+// var zero = document.getElementById("btnzero")
+// zero.addEventListener("click", function(){
+// 	rightInput.value += 0
+	
+// })
+
+// var decimal = document.getElementById("btndecimal")
+// decimal.addEventListener("click", function(){
+// 	rightInput.value = "."
+
+// })
+
+
+
+
+//______________________Operator Functions_______________________//
+
+// var operator;
+// 	function setValues()
+// 	{
+// 		leftInput = Number(document.getElementById("leftInput").value);
+// 		operator = Number(document.getElementById("operator").value);
+// 	}
+
+// 	function sum() {
+// 		setValues();
+// 		results = a+b;
+// 	}
+
+
+// var clear_all = document.getElementById("clear_all")
+// clear_all
 
 
 
