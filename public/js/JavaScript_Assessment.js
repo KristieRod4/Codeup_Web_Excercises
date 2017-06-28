99641463c38d2b0de261d92b08ec258dccb07dde
Kristie Rodriguez <kristierodriguez@Kristies-MacBook-Pro.local>
2017-06-28 17:03:46 -0500

    Adding updates to Konami exercise and JavaScript Assessment.

--- public/js/JavaScript_Assessment.js
+++ public/js/JavaScript_Assessment.js
@@ -8,13 +8,15 @@
 //  isNegative(-1) // true
 //  isNegative(6) // false
 
-function isNegative(number){
-if (number > 0) {
+
+function isNegative(input){
+if (input < 0) {
         return true;
-    }
+    } else{
     return false;
 }
-
+    isNegative(-1) 
+    isNegative(6) 
 	console.log(isNegative(number));
 
 // Write a function named average that accepts an array of numbers and returns the average of those numbers
@@ -22,15 +24,17 @@ if (number > 0) {
 //  average([1, 2, 3]) // 2
 //  average([4, 6, 8, 10, 12]) // 8
 
-function average(getAverage){
-	var = getAverage(int[] array)
+function average(array){
+	var getAverage = 0;
+    for(i = 0; i < array.length; i++){
+        getAverage += array[i];
+    }
+        return total / array.length;
 
-    int sum = 0;
-    for(int i : array) sum += i;
-    return ((double) sum)/array.length;
-}
+    
 
-	console.log()
+	console.log(getAverage[1, 2, 3]);
+    console.log(getAverage[4, 6, 8]);
 
 
 
@@ -41,18 +45,18 @@ function average(getAverage){
 //  countOdds([4, 6, 8, 10]) // 0
 //  countOdds([1, 2, 1, 1]) // 3
 
-var = someNumber[
-	{countOdds: 4, 6, 8, 10}
-
-]
-
-
-function countOdds(someNumber) {
-    if (someNumber % 2 === 1) {
-        return true;
+function countsOdds(array){
+    var total = 0;
+    for (var i = 0; i < array.length; i++){
+        if (array[i] % 2 != 0) {
+            total ++;
+        }
     }
-    return false;
+    return total;
 }
+console.log(countsOdds([1, 2, 3]));
+console.log(countsOdds([4, 6, 8, 10]));
+console.log(countsOdds([1, 2, 1, 1]));
 
 
 // Write a function named convertNameToObject that accepts a string that contains a first name and last name 
@@ -61,15 +65,26 @@ function countOdds(someNumber) {
 //  convertNameToObject('Harry Potter') // {firstName: 'Harry', lastName: 'Potter'}
 //  convertNameToObject('Ron Weasley') // {firstName: 'Ron', lastName: 'Weasley'}
 
-function convertNameToObject(){
-
+function convertNameToObject(string){
+    var nameArray = string.split(" ");
+    var nameObject = {
+        firstName : nameArray[0],
+        lastName : nameArray[1]
+    }; 
+    return nameObject;
 }
 // Write a function named fiveTo that accepts a number and returns an array of the integers from 5 up to (but not including) the passed number.
 
 //  fiveTo(10) // [5, 6, 7, 8, 9]
 //  fiveTo(6) // [5]
 
-function fiveTo(){
+function fiveTo(number){
+    var arrayIntegers = [];
+    for (var i = 5; i < number; i++) {
+        arrayIntegers.push(i);
+
+    }
+    return arrayIntegers;
 
 }
 // Write a function named upperCaseLastNames that accepts an array of objects where each object has 
@@ -81,18 +96,21 @@ function fiveTo(){
      {firstName: 'Ron', lastName: 'weasley'},
      {firstName: 'Hermione', lastName: 'granger'}
  ];
- upperCaseLastNames(names);
+ // upperCaseLastNames(names);
  
- [
-    {firstName: 'Harry', lastName: 'Potter'},
-    {firstName: 'Ron', lastName: 'Weasley'},
-    {firstName: 'Hermione', lastName: 'Granger'}
+ // [
+ //    {firstName: 'Harry', lastName: 'Potter'},
+ //    {firstName: 'Ron', lastName: 'Weasley'},
+ //    {firstName: 'Hermione', lastName: 'Granger'}
 
- ] 
+ // ] 
 
- function upperCaseLastNames(){
- 	
+ function upperCaseLastNames(names){
+    names.forEach(function(name){
+        name.lastName = name.lastName.charAt(0).toUpperCase() + name.lastName.slice(1);
+    });
+    return names;
  }
+console.log(upperCaseLastNames(names));
 
-
-});
+}());
