2cfd1e331cb06dc643741c6febcf2309834437b5
Kristie Rodriguez <kristierodriguez@Kristies-MacBook-Pro.local>
2017-07-07 16:57:07 -0500

    Adding updates to Weather Map API exercise.

---
+++ public/js/weather_map_api.js
@@ -0,0 +1,51 @@
+(function(){
+"use strict";
+
+
+// ====================================Lat/Lon Button========//
+
+		// $(".button").click(function)(getData){
+  // 			var findLat = document.getElementById('boxLat').value;
+  // 			var findLon = document.getElementById('boxLon').value;
+  // 				findLat = parseFloat(findLat);
+  // 				findLon = parseFloat(findLon);
+
+  		// };
+
+
+
+ //======================================Map Info===============================//               
+
+       var mapOptions = {
+           zoom: 10,
+              center: {
+              lat: 29.4964645,
+              lng: -98.478578
+          }
+
+       };
+
+       var  mapElement= new google.maps.Map(document.getElementById("map"), mapOptions);
+
+ 
+  
+
+               
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})();
